40d883077fa05cf3c9b76bc8d56733ba70116c24
FareedJsp01~ <fareedjosip@gmail.comm>
2022-11-22 11:01:23 +0800

    i

--- resources/views/driver/edit.blade.php
+++ resources/views/driver/edit.blade.php
@@ -11,10 +11,24 @@
   @csrf
 
 <div class="container-fluid">
+
+  <div class="row page-titles mx-0">
+    <div class="col-sm-6 p-md-0">
+        <div class="welcome-text">
+            <h4>Hi, welcome back!</h4>
+            <p class="mb-0">Add Driver Data</p>
+        </div>
+    </div>
+    <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="javascript:void(0)">Vehicle</a></li>
+            <li class="breadcrumb-item active"><a href="javascript:void(0)">Add Data</a></li>
+        </ol>
+    </div>
+  </div>
+  
   <div class="row">
     <div class="col-6">
-
-      <h4 class="mb-3 mt-3">Update Driver Data</h4>
 
       <div class="mb-3">
         <label class="form-label">Company</label>
--- resources/views/vehicle/create.blade.php
+++ resources/views/vehicle/create.blade.php
@@ -11,23 +11,24 @@
   @csrf
 
 <div class="container-fluid">
-  <div class="row">
-    <div class="col-6">
 
-      <div class="row page-titles mx-0">
-        <div class="col-sm-6 p-md-0">
-            <div class="welcome-text">
-                <h4>Hi, welcome back!</h4>
-                <p class="mb-0">Add Driver Data</p>
-            </div>
-        </div>
-        <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="javascript:void(0)">Vehicle</a></li>
-                <li class="breadcrumb-item active"><a href="javascript:void(0)">Add Data</a></li>
-            </ol>
+  <div class="row page-titles mx-0">
+    <div class="col-sm-6 p-md-0">
+        <div class="welcome-text">
+            <h4>Hi, welcome back!</h4>
+            <p class="mb-0">Add Driver Data</p>
         </div>
     </div>
+    <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="javascript:void(0)">Vehicle</a></li>
+            <li class="breadcrumb-item active"><a href="javascript:void(0)">Add Data</a></li>
+        </ol>
+    </div>
+</div>
+
+  <div class="row">
+    <div class="col-6">
 
         @php
             $company = DB::table('users')->first();
