8b4db2244f6578fcb7d275adff122c9986ff5b6a
FareedJsp01~ <fareedjosip@gmail.comm>
2022-11-25 13:54:24 +0800

    asc

--- app/Http/Controllers/PetrolController.php
+++ app/Http/Controllers/PetrolController.php
@@ -13,7 +13,7 @@ class PetrolController extends Controller
 {
     public function index()
     {
-        $petrol = Petrol::orderBy('fill_date_time', 'desc')->get();
+        $petrol = Petrol::orderBy('fill_date_time', 'asc')->get();
         return view('petrol.index', compact('petrol'));
     }
 
