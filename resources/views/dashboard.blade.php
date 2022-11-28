542f040885dc8c5c2a6d5954c1d24b61dfee56e6
Fareed <fareedjosip@gmail.com>
2022-11-28 22:48:29 +0800

    dashboard head

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,17 @@
 @endphp
 
 <div class="container-fluid">
+    <div class="form-head mb-sm-5 mb-3 d-flex flex-wrap align-items-center">
+        <h2 class="font-w600 title mb-2 mr-auto ">Dashboard</h2>
+        <div class="weather-btn mb-2">
+            <span class="font-w600 text-black">
+                <i class="fa-solid fa-clock"></i>
+                {{
+                    Carbon::now()->toDateTimeString();
+                }}
+            </span>
+        </div>
+    </div>
     <div class="row">
         <div class="col">
             <div class="card">
